f3f99ce77b52618c7e920f957c2ed77a9905cac0
5115-Source <chegstad5115@student.whatcom.edu>
2025-01-27 21:22:02 -0800

    Updated some logic. Still needs a proper gameboard printing method that works if called at any time in the program.

--- MemoryCardGame.py
+++ MemoryCardGame.py
@@ -20,8 +20,6 @@ Use another data type for storing the game board and the card states
 Use a stack of cards to check each "index" one by one and see if we need to update the given card to a true or false flip state
 
 """
-
-#Main method
 import random
 
 #Linked list class
@@ -34,77 +32,78 @@ class Node:
 
 
 def main():
-    #Need to create a more instructive introduction
-    print("Welcome to the memory card game!")
+    #ChatGPT helped with instructions and understanding python formatting and problem solving
+    print("The memory card game, also known as Concentration, is a card-matching game where all cards are laid face down.")
+    print("You will take turns flipping over two cards at a time, trying to find matching pairs.")
+    print("The goal is to remember the locations of cards to match them and collect pairs until all cards are matched.")
+    print("Once you match all the cards and have none flipped upside down you win!\n")
     
     deck = deckBuilder()
-    #print(deck)
-
-    #win = False
-
     head = None
+    gameRunning = True
     
-    
-    #while win != True:
-    
-    #While the deck still has cards create and print the game board
+    #Assemble gameboard
     while deck:
         gameBoard(head, deck)
-        
-    inputChecker(deck)
     
-#Build a "shuffled" stack of cards that has 2 of each number in it (0-9 each number occurs twice)
+    while gameRunning == True:
+        inputChecker(deck)
+        #gameBoard(head, deck)
+        
+    #if all cards are flipped stop the game and add a congratulations message
+    
+#Build a "shuffled" stack of 20 cards that has 2 of each symbol in it in it
 def deckBuilder():
     deck = []
     
-    """#fill the deck with cards and a flipped state of false
-    for i in range(10):
-        #Using a stack to add a pair of each number to the deck
-        deck.append((i, False))
-        deck.append((i, False))
-    random.shuffle(deck)
-    return(deck)"""
-    
-    # List of symbols that will be use for pairs of cards
-    symbols = ['!', '@', '#', '$', '%', '<', '&', '*', '>', '+']
+    # List of 2 of each symbol that will be used as pairs of cards
+    symbols = ['!', '@', '#', '$', '%', '<', '&', '*', '>', '+', 
+               '!', '@', '#', '$', '%', '<', '&', '*', '>', '+']
+    random.shuffle(symbols)
 
-    #Using a stack to add a pair of each symbol to the deck
-    number = 20
+    #List of numbers from 1 - 20
+    numbers = list(range(1, 21))
+
     for symbol in symbols:
-        #Need to adjust to get the numbers in order and have the symbols in random order
-        deck.append((symbol, number))
-        number -= 1
-        deck.append((symbol, number))
-        number -= 1
-    random.shuffle(deck)
-    #For troubleshooting
-    print(deck)
+        deck.append((symbol, numbers.pop()))
+    
     return(deck)
 
 def gameBoard(head, deck):
     currentCard = Node(deck.pop())
     currentCard.next = head
     
-    cardNumber = len(deck) + 1
-    if isinstance(currentCard.data[1], int):
+    cardNumber = currentCard.data[1]
+    if isinstance(cardNumber, int):
         if cardNumber > 9:
-            print(f"{cardNumber}", end = " ")
+            print(f"{cardNumber}", end = "  ")
         else:
-            print(f" {cardNumber}", end = " ")
+            print(f" {cardNumber}", end = "  ")
     else: 
-        print(f" {currentCard.data[0]}", end = " ")
+        print(f" {currentCard.data[0]}", end = "  ")
         
     #If length of the stack is divisible by 5 start a new row
     if len(deck) % 5 == 0:
-        print()
+        print("\n")
         
 #Unfinished
 def inputChecker(deck):
-    card1 = input("What is the number of the first card you want to flip?")
-    #Update that card to have a flipped state of true
+    cardFlip1 = input("What is the number of the first card you want to flip?")
+    while not cardFlip1.isdigit() or not 1 <= int(cardFlip1) <= 20:
+        cardFlip1 = input("Sorry that wasn't an integer number between 1-20. What is the number (1-20) of the first card you want to flip?")
     
-    card2 = input("What is the number of the second card you want to flip?")
-    #Update that card to have a flipped state of true
+    cardFlip2 = input("What is the number of the second card you want to flip?")
+    while not cardFlip2.isdigit() or not 1 <= int(cardFlip2) <= 20:
+        cardFlip2 = input("Sorry that wasn't an integer number between 1-20. What is the number (1-20) of the second card you want to flip?")
+
+    #Flip both cards
+    
+    #Display the gameboard
+
+    if cardFlip1 != cardFlip2:
+        #Flip the cards back
+        #Display the gameboard
+        print()
 
     return deck
 
